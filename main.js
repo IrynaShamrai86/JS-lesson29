781429881b3b85c531189ba734841ae65e902131
“Iryna.Shamrai86” <irina.shamray86@gmail.com>
2022-11-20 14:03:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// let carModel = 'Volkswagen';
+// let category = 'Golf';
+// let fuel = 'Disel';
+// let color = 'White';
+//
+// console.log(carModel,category,fuel,color);
+
+
+// let carModel = prompt('Яка марка авто?');
+// let category = prompt('Яка модель авто?');
+// let fuel = prompt('Який тип палива?');
+// let color = prompt('Якого кольору авто?');
+//
+// console.log(carModel,category,fuel,color);
+
+
+/*
+
+ */
+
+
+// let name = prompt('Як тебе звати?');
+// alert(`Привіт, ${name}!`);
+//
+// let year = prompt('Рік твого народження?');
+// const currentYear = 2022;
+// console.log(currentYear - year);
+//
+// let side = prompt('Яка довжина сторони квадрата?');
+// console.log(side * 4);
+
+
+let radius = prompt('Який радіус кола?');
+console.log(2 * 3.14 * radius);
+
+
+let distance = prompt('Яка відстань між точками А та Б в кілометрах?');
+let time = prompt('За скільки годин ти плануєш дістатися точки Б?');
+console.log(distance / time);
+
+
+let $ = prompt('У мене є така сума $');
+const exchangeRateUSD = 36.5686;
+const exchangeRateEUR = 37.7681;
+console.log(exchangeRateUSD / exchangeRateEUR * $);
+
+
+let $ = prompt('У мене є така сума $');
+const exchangeRate$ = 36.5686;
+console.log(exchangeRate$ * $);
